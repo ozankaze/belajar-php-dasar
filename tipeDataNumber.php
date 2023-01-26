034ba748e6c714337598628b13411c997028731b
ozankaze <ozankaze101@gmail.com>
2023-01-26 15:13:55 +0700

    belajar PHP Dasar

---
+++ tipeDataNumber.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "Decimal : ";
+var_dump(1234);
+
+echo "Octan : ";
+var_dump(01234);
+
+echo "Hexadecimal : ";
+var_dump(0x345);
+
+echo "Binary : ";
+var_dump(0b1111);
+
+echo "Underscore in number : ";
+var_dump(8_912_827_381);
+
+echo "Floating Point : ";
+var_dump(1.234);
+
+echo "Floating Point Dengan E Notation Plus : (1.7 x 1000) : ";
+var_dump(1.7e3);
+
+echo "Floating Point Dengan E Notation Minus : (1.7 x 0.001) : ";
+var_dump(1.7e-3);
+
+echo "Underscore di Floating Point : ";
+var_dump(1_234.65);
