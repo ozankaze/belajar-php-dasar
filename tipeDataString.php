bedc848f2efdfd430ef5a1ce09dba9b86098496d
ozankaze <ozankaze101@gmail.com>
2023-01-27 11:08:44 +0700

    belajar PHP Dasar

---
+++ tipeDataString.php
@@ -0,0 +1,21 @@
+<?php
+
+echo 'Name : ';
+echo 'Ozan Kaze';
+echo "\n";
+
+echo "Name : ";
+echo "Ozan\t Kaze\n";
+
+echo <<<END
+ini adalah cara ke-3 membuat string
+menggunakan HEREDOC
+selamat belajar
+
+END;
+
+echo <<<'END'
+ini adalah cara ke-4 membuat string
+menggunakan NOWDOC
+selamat belajar
+END;
