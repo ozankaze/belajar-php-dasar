283c60c3b1d940e145929ad683d3af7a89bb3609
ozankaze <ozankaze101@gmail.com>
2023-01-28 17:41:59 +0700

    belajar PHP Dasar

---
+++ operatorAritmatika.php
@@ -0,0 +1,31 @@
+<?php
+
+$k = 30;
+$l = 4;
+$p = 9;
+$o = 80;
+$z = 2;
+
+$result = $k + $l;
+var_dump($result);
+
+$resultNegative = -$result;
+var_dump($resultNegative);
+
+$resultPositive = +$result;
+var_dump($resultPositive);
+
+$resultPengurangan = $o - $p;
+var_dump($resultPengurangan);
+
+$resultPerkalian = $l * $p;
+var_dump($resultPerkalian);
+
+$resultPembagian = $o / $z;
+var_dump($resultPembagian);
+
+$resultSisaBagi = $o / $p;
+var_dump($resultSisaBagi);
+
+$resultPangkat = $l ** $z;
+var_dump($resultPangkat);
