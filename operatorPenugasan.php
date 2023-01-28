32ba96e7da015dd04d146a63a9aa2f6eedbcecde
ozankaze <ozankaze101@gmail.com>
2023-01-28 20:40:30 +0700

    belajar PHP Dasar

---
+++ operatorPenugasan.php
@@ -0,0 +1,13 @@
+<?php
+
+$total = 0;
+
+$semangka = 10000;
+$anggur = 50000;
+$jeruk = 90000;
+
+$total += $semangka;
+$total += $anggur;
+$total += $jeruk;
+
+var_dump($total);
---
+++ operatorPerbandingan.php
@@ -0,0 +1,7 @@
+<?php
+
+var_dump("30" == 30);
+var_dump("30" === 30);
+var_dump(10 < 9);
+var_dump(9 >= 9);
+var_dump(10 != 8);
