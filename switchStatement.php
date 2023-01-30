d28f6fb6d6cb1968154c98050ea7f99a09a45533
ozankaze <ozankaze101@gmail.com>
2023-01-30 21:58:18 +0700

    belajar PHP Dasar

---
+++ switchStatement.php
@@ -0,0 +1,18 @@
+<?php
+
+$nilai = "E";
+
+switch ($nilai) {
+    case "A":
+        echo "Anda lulus dengan baik" . PHP_EOL;
+        break;
+    case "B":
+    case "C":
+        echo "Anda lulus" . PHP_EOL;
+        break;
+    case "D":
+        echo "Anda tidak lulus" . PHP_EOL;
+        break;
+    default:
+        echo "Anda salah jurusan" . PHP_EOL;
+}
