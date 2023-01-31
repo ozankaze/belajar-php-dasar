16ea5570c9a480487c0c91efc59a4c05cdd8fd81
ozankaze <ozankaze101@gmail.com>
2023-01-31 14:41:15 +0700

    belajar PHP Dasar

--- switchStatement.php
+++ switchStatement.php
@@ -16,3 +16,18 @@ switch ($nilai) {
     default:
         echo "Anda salah jurusan" . PHP_EOL;
 }
+
+switch ($nilai):
+    case "A":
+        echo "Anda lulus dengan baik" . PHP_EOL;
+        break;
+    case "B":
+    case "C":
+        echo "Anda lulus" . PHP_EOL;
+        break;
+    case "D":
+        echo "Anda tidak lulus" . PHP_EOL;
+        break;
+    default:
+        echo "Anda salah jurusan" . PHP_EOL;
+endswitch;
---
+++ ternaryOperator.php
@@ -0,0 +1,17 @@
+<?php
+
+$gender = "LAKI";
+
+// $hi = null;
+
+// if ($gender == "PRIA") {
+//     $hi = "Hi bro";
+// } else {
+//     $hi = "Hi nona";
+// }
+
+// echo $hi . PHP_EOL;
+
+$hi = $gender == "PRIA" ? "Hi bro" : "Hi nona";
+
+echo $hi . PHP_EOL;
