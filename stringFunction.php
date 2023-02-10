1cec2cafa347caa4414368588fcbab205a10d084
ozankaze <ozankaze101@gmail.com>
2023-02-10 15:41:33 +0700

    belajar PHP Dasar

--- stringFunction.php
+++ stringFunction.php
@@ -1,3 +1,13 @@
 <?php
 
 var_dump(join(".", [5, 6, 7, 8, 9, 10]));
+var_dump(explode(" ", "Returns an array of strings"));
+var_dump(strtolower("RETURNS AN ARRAY OF STRINGS"));
+var_dump(strtoupper("returns an array of strings"));
+var_dump(str_shuffle("abcdefghijklmn"));
+
+
+$str = "abcdefgh";
+
+$strFinal = str_shuffle($str);
+echo $strFinal . PHP_EOL;
