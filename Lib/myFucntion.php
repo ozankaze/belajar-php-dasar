601f5ff34549a94424893d336591d737567bd5b1
ozankaze <ozankaze101@gmail.com>
2023-02-11 20:05:21 +0700

    belajar PHP Dasar

---
+++ Lib/myFucntion.php
@@ -0,0 +1,5 @@
+<?php
+
+function sayHello(string $firstName, string $lastName) {
+    return "Hello $firstName $lastName" . PHP_EOL;
+}
---
+++ requireAndIncluded.php
@@ -0,0 +1,5 @@
+<?php
+
+include "Lib/myFucntion.php";
+
+echo sayHello("Eunha", "Eva");
